242d2c6c6783fac3310768c5620c737c968918f9
Alex Odle <alex.odle@gmail.com>
2019-12-17 14:55:22 -0800

    alert ui

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -5,8 +5,27 @@ const DEFAULT_TIMEOUT_MS = 5000
 
 const useStyles = makeStyles({
   root: {
-
-  }
+    position: 'relative',
+    'padding-left': '1em',
+    'margin': '2em',
+    'border-radius': '5px',
+    border: '1px solid transparent',
+  },
+  success: {
+    'background-color': '#d4edda',
+    'border-color': '#c3e6cb',
+    color: '#155724',
+  },
+  info: {
+    'background-color': '#e2e3e5',
+    'border-color': '#d6d8db',
+    color: '#383d41',
+  },
+  error: {
+    'background-color': '#f8d7da',
+    'border-color': '#f5c6cb',
+    color: '#721c24',
+  },
 })
 
 interface AlertInfo {
@@ -29,25 +48,21 @@ export const AlertContext = createContext<AlertContextProps>({
 
 export const Alert: FunctionComponent<{}> = ({ children }) => {
   const [alert, setAlert] = useState<AlertInfo | null>(null)
+  const classes = useStyles()
 
-  function hide() {
+  function onAlert(message: string, status: AlertStatus) {
     if (alert) {
       clearTimeout(alert.timeoutId)
       setAlert(null)
     }
-  }
-
-  function onAlert(message: string, status: AlertStatus) {
-    hide()
     const timeoutId = setTimeout(() => setAlert(null), DEFAULT_TIMEOUT_MS)
     setAlert({ message, status, timeoutId })
   }
 
   const renderAlert = () => (
     !alert ? null :
-      <div className={`datenight-alert ${alert.status}`}>
+      <div className={`${classes.root} ${classes[alert.status]}`}>
         <p>{alert.message}</p>
-        <span className='close-button' onClick={hide}>X</span>
       </div>
   )
 
